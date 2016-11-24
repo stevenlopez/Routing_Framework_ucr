0169f8b50e37ae8927127393c93a06790e568067
stevenlopez <slope029@ucr.edu>
2016-11-23 21:22:19 -0800

    Update segmentgroup.cc

--- Source/segmentgroup.cc
+++ Source/segmentgroup.cc
@@ -1,3 +1,16 @@
+#include "../Headers/segmentgroup.h"
+#include "../Headers/claim.h"
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+bool Utilities::SegmentGroup::on_segment(Point p1,Point p2,Point p3) {
+    if((p2.x <= std::max(p1.x,p3.x) && p2.x >= std::min(p1.x,p3.x)) &&
+            (p2.y <= std::max(p1.y,p3.y) && p2.y >= std::min(p1.y,p3.y))) {
+        return true;
+    }
     return false;
 }
 
@@ -17,8 +30,8 @@ unsigned Utilities::SegmentGroup::size() const {
     return this->segments.size();
 }
 
-PathSegment* Utilities::SegmentGroup::at(unsigned index) const {
-    return this->segments.at(index);
+PathSegment* Utilities::SegmentGroup::at(unsigned index) const { 
+    return this->segments.at(index); 
 }
 
 bool Utilities::SegmentGroup::empty() const {
@@ -44,16 +57,62 @@ int Utilities::SegmentGroup::get_length() {
 
 void Utilities::SegmentGroup::print() {
     if(this->segments.size() <= 0) {
-        cout << "Empty path" << endl;
+	cout << "Empty path" << endl;
         //return;
     }
     else{
     for(unsigned i = this->segments.size() - 1; i > 0; --i) {
         this->segments.at(i)->print();
-        Point check = this->segments.at(i)->get_source();
+	Point check = this->segments.at(i)->get_source();
         if((i > 0) && (check.x != -10)) {//prints in the reverse direction
             cout << " | ";
         }
     }
     //cout << " | ";
-"segmentgroup.cc" 118L, 3594C                                                                                                                                                                                                      66,2-9        21%
+    this->segments.at(0)->print();
+    cout << endl;
+    }
+}
+
+void Utilities::SegmentGroup::add_segment(PathSegment* pathsegment) {
+    this->segments.push_back(pathsegment);
+}
+
+void Utilities::SegmentGroup::add_segment(Point source, Point sink) {
+    this->segments.push_back(new PathSegment(source, sink));
+}
+
+/* Note: if the pathsegment appears multiple times in the path, all instances will be removed */
+void Utilities::SegmentGroup::remove_segment(PathSegment* pathsegment) {
+    for(vector<PathSegment*>::iterator it = this->segments.begin(); it != this->segments.end(); it++) {
+        if((*it) == pathsegment) {
+            this->segments.erase(it);
+            if(it == this->segments.end()) { break; }
+        }
+    }
+    delete pathsegment;
+}
+
+/* Note: if the pathsegment appears multiple times in the path, all instances will be removed */
+void Utilities::SegmentGroup::remove_segment(Point source, Point sink) {
+    PathSegment* to_delete;
+    for(vector<PathSegment*>::iterator it = this->segments.begin(); it != this->segments.end(); it++) {
+        if((*it)->get_source() == source && (*it)->get_sink() == sink) {
+            to_delete = (*it);
+            this->segments.erase(it);
+            if(it == this->segments.end()) { break; }
+        }
+    }
+    if(to_delete != NULL) { delete to_delete; }
+}
+
+void Utilities::SegmentGroup::remove_segment(int index) {
+    if(index >= this->segments.size()) {
+        claim("Attempting to remove segment with index out of range of the paths size", kError);
+    }
+    vector<PathSegment*>::iterator it = this->segments.begin();
+    it += index;
+    PathSegment* to_remove = *it;       //TODO: Double check that this *it is correct
+    this->segments.erase(it);
+    delete to_remove;
+}
