0a4eac36d3042ac884aa7bc7ae9f604e22b8cd3d
stevenlopez <slope029@ucr.edu>
2016-11-23 21:21:53 -0800

    Update pathsegment.cc

--- Source/pathsegment.cc
+++ Source/pathsegment.cc
@@ -1,3 +1,34 @@
+#include "../Headers/pathsegment.h"
+#include "../Headers/claim.h"
+
+#include <iostream>
+
+using std::cout;
+using Utilities::Point;
+
+void Utilities::PathSegment::update_length() {
+    if(this->source.x == this->sink.x && this->source.y == this->sink.y) {
+        claim("You have entered two equal points, PathSegment length is zero", kWarning);
+        this->length = 0;
+    }
+    if(this->source.x == this->sink.x) {
+        if(source.y > sink.y) {
+            this->length = this->source.y - this->sink.y;
+        } else {
+            this->length = this->sink.y - this->source.y;
+        }
+    }
+    else if(this->source.y == this->sink.y) {
+        if(this->source.x > this->sink.x) {
+            this->length = this->source.x - this->sink.x;
+        } else {
+            this->length = this->sink.x - this->source.x;
+        }
+    }
+    else {
+        claim("PathSegment being created does not represent a straight line segment", kError);
+    }
+}
 
 Utilities::PathSegment::PathSegment(Point source, Point sink):
         source(source.x,source.y),sink(sink.x,sink.y) {
@@ -40,7 +71,7 @@ bool Utilities::PathSegment::contains_points(Point source, Point sink) {
 }
 
 void Utilities::PathSegment::print_source() {
-        cout << "(" << this->source.x << "," << this->source.y << ")";
+    	cout << "(" << this->source.x << "," << this->source.y << ")";
 }
 void Utilities::PathSegment::print_sink() {
     cout << "(" << this->sink.x << "," << this->sink.y << ")";
@@ -48,12 +79,23 @@ void Utilities::PathSegment::print_sink() {
 
 void Utilities::PathSegment::print() {
     if(this->sink.x == -10){
-            cout << "\n";
+	    cout << "\n";
     }
     else{
-        this->print_sink();
-        cout << " -> ";
-        this->print_source();
-        //print sink to source
+    	this->print_sink();
+   	cout << " -> ";
+    	this->print_source();
+    	//print sink to source
     }
-                                                                                                                                                                                                                                   49,4          72%
+}
+
+void Utilities::PathSegment::set_source(Point source) {
+    this->source = source;
+    update_length();
+}
+
+void Utilities::PathSegment::set_sink(Point sink) {
+    this->sink = sink;
+    update_length();
+}
+
