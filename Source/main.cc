16771c77b44bb74370e402f120179983c6773d84
stevenlopez <slope029@ucr.edu>
2016-11-23 20:21:57 -0800

    Update main.cc

--- Source/main.cc
+++ Source/main.cc
@@ -1,4 +1,4 @@
-
+#include "../Headers/grid2.h"
 #include "../Headers/grid.h"
 #include "../Headers/problem_object.h"
 #include <time.h>
@@ -6,49 +6,54 @@
 #include <iostream>
 
 using std::cerr;
+using std::cin;
 using std::cout;
 using std::endl;
 
 int main(int argc,char* argv[]) {
 
-	// DO NOT CHANGE THIS SECTION OF CODE
-	if(argc < 2) { 
-		cout << "Usage: ./grid_router <test_file>" << endl; 
-		exit(1);
-	}
-	Utilities::ProblemObject* first_problem = new Utilities::ProblemObject(std::string(argv[1]));
-	// EDIT FROM HERE DOWN
+        // DO NOT CHANGE THIS SECTION OF CODE
+        if(argc < 2) {
+                cout << "Usage: ./grid_router <test_file>" << endl;
+                exit(1);
+        }
+        Utilities::ProblemObject* first_problem = new Utilities::ProblemObject(std::string(argv[1]));
+        // EDIT FROM HERE DOWN
 
-	//Create your problem map object (in our example, we use a simple grid, you should create your own)
-	Grid g(first_problem);
+        //Create your problem map object (in our example, we use a simple grid, you should create your own)
+        Grid2 g(first_problem);
 
-	/*
-	Note: we do not take into account the connections or blockers that exist in the Project Object
-	You should be accouting for these in your problem map objects (you should not be using Grid). You
-	should be using both the Node and Edge classes as the background for whatever problem map object
-	you create.
-	*/
+        /*
+        Note: we do not take into account the connections or blockers that exist in the Project Object
+        You should be accouting for these in your problem map objects (you should not be using Grid). You
+        should be using both the Node and Edge classes as the background for whatever problem map object
+        you create.
+        */
 
-	/*
-	Run your algorithm after creating your problem map object. You should be returning from your algorithm 
-	either a Path or a Netlist
+        /*
+        Run your algorithm after creating your problem map object. You should be returning from your algorithm 
+        either a Path or a Netlist
 
-	Path: a series of straight line segments, with a single source and a single sink
-	Netlist: a series of stright line segments, with a single source and more than one sink
-	*/
-	vector<Path*> paths = g.test_algorithm();
-
-	//Print the paths/netlists that you return from your algorithm
-	for(unsigned i = 0; i < paths.size(); i++) {
-		cout << "Path " << i << ": ";
-		paths.at(i)->print();
-		Path* temp = paths.at(i);
-		delete temp;
-	}
-
-	paths.clear();
-
-	delete first_problem;
-
-	return 0;
-}
+        Path: a series of straight line segments, with a single source and a single sink
+        Netlist: a series of stright line segments, with a single source and more than one sink
+        */
+        char inter;
+        bool intersections;
+        cout << "Intersections allowed(y or n)? " << endl;
+        cin >> inter;
+        if(inter == 'y'){
+                intersections = true;
+        }
+        else{
+                intersections = false;
+        }
+        //g.test_algorithm();
+        vector<Path*> paths = g.test_algorithm(intersections);
+        //call test algorithm here
+        std::cout << endl;
+        //Print the paths/netlists that you return from your algorithm
+        for(unsigned i = 0; i < paths.size(); i++) {
+                        //cout << paths.size() << endl;
+                        cout << "Path " << i << ": ";
+                        paths.at(i)->print();
+"main.cc" 68L, 1935C                                                                                                                                                                                                               18,7-21       Top
